384b5127d83359f1d6feb916f7823a48edf659b8
Vishwas-Agrawal <72984613+Vishwas-Agrawal@users.noreply.github.com>
2021-07-05 15:32:05 +0530

    Update Signoutbtn.js

--- client/src/components/Signoutbtn.js
+++ client/src/components/Signoutbtn.js
@@ -1,13 +1,17 @@
-import React, { useContext } from 'react'
-import { Context as AuthContext } from "../context/AuthContext"
+import React, { useContext } from "react";
+import { Context as AuthContext } from "../context/AuthContext";
 import { useHistory } from "react-router-dom";
 
 export default function Signoutbtn() {
-    let history = useHistory();
-    const { signout } = useContext(AuthContext);
-    return (
-        <div className="signoutbutton" onClick={()=>signout(history)}>
-            SIGN OUT
-        </div>
-    )
+  let history = useHistory();
+  const { signout } = useContext(AuthContext);
+  return (
+    <div className="signoutdiv">
+      <div className="signoutbutton" onClick={()=>{history.push("/home")}}>HOME</div>
+      <div className="signoutbutton" onClick={() => signout(history)}>
+        SIGN OUT
+      </div>
+    </div>
+  );
 }
+
